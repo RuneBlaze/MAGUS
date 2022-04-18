61d60d1974bd546b9129a3d043cef8250d4784d8
runeblaze <runeblaze@excite.co.jp>
2022-04-18 15:55:10 -0500

    added subset accuracy script

--- scripts/subset_acc.jl
+++ scripts/subset_acc.jl
@@ -13,12 +13,14 @@ end
 addprocs(WORKERS)
 
 @everywhere TRUE_ALN_FILENAME = "true.indel.fa"
+# @everywhere TRUE_ALN_FILENAME = "truealign.hf.txt"
 
 @everywhere function parse_result(io :: String, keys)
     res = Dict()
     for l = eachline(IOBuffer(io))
         for k = keys
             if contains(l, k)
+                # @show "contains!"
                 src = split(l)[2]
                 r = tryparse(Float64, src)
                 res[k] = isnothing(r) ? src : r
@@ -30,8 +32,9 @@ end
 
 @everywhere function obtain_rates(ref, estimated)
     fastsp_script = joinpath(@__DIR__, "restricted_fastsp.py")
-    res = read(run(`python3 $fastsp_script -r $ref -e $estimated`), String)
+    res = read(`python3 $fastsp_script -r $ref -e $estimated`, String)
     r = parse_result(res, ["SPFP", "SPFN"])
+    # @show res
     return (spfp = r["SPFP"], spfn = r["SPFN"])
 end
 
@@ -57,8 +60,8 @@ tasks = []
 for arg in ARGS
     for p in glob(arg)
         graph_path = joinpath(p, "graph")
-        subset_path = joinpath(p, "decomposition")
-        subset_alignment_pattern = joinpath(subset_path, "subset_*.txt")
+        subset_path = joinpath(p, "subalignments")
+        subset_alignment_pattern = joinpath(subset_path, "subalignment_subset_*.txt")
         graph_alignment_pattern = joinpath(graph_path, "backbone_*_mafft.txt")
         tokens = splitpath(p)
         dataset = tokens[1]
