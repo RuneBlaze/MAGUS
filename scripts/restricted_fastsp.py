04327cc7942c629cca9b77466c63661ee9e4105e
runeblaze <runeblaze@excite.co.jp>
2022-04-25 20:24:57 -0500

    tried removing all gap sites

--- scripts/restricted_fastsp.py
+++ scripts/restricted_fastsp.py
@@ -1,5 +1,7 @@
 import argparse
 from Bio import AlignIO, SeqIO
+from Bio.Seq import Seq
+import numpy as np
 import os
 
 parser = argparse.ArgumentParser(description='Run FastSP on a restricted set of sequences.')
@@ -19,14 +21,31 @@ filtered = [record for record in est_alignment if record.id in ref_ids]
 est_ids = set([record.id for record in est_alignment if record.id in ref_ids])
 filtered_ref = [record for record in ref_alignment if record.id in est_ids]
 
+def compress_alignment(input, output):
+    A = AlignIO.read(open(input), "fasta")
+    removable_idx = [] # removable column idx
+    N = A.get_alignment_length()
+    for i in range(N):
+        row = A[:, i]
+        if row.count('-') == len(row):
+            removable_idx.append(i)
+    for record in A:
+        stringview = np.array(list(record.seq))
+        stringview = np.delete(stringview, removable_idx)
+        record.seq = Seq(''.join(list(stringview)))
+    with open(output, "w+") as fh:
+        AlignIO.write(A, fh, "fasta")
+
 if len(filtered) <= 1:
   print("")
   exit()
 
 # write out the estimated alignment
+# restricted_ = args.est + ".restricted_"
 restricted = args.est + ".restricted"
 restricted_ref = args.est + ".rref"
 SeqIO.write(filtered, restricted, "fasta")
 SeqIO.write(filtered_ref, restricted_ref, "fasta")
-os.system("java -jar FastSP.jar -e %s -r %s" % (restricted, restricted_ref))
+compress_alignment(restricted_ref, restricted_ref + "_")
+os.system("java -jar FastSP.jar -e %s -r %s" % (restricted, restricted_ref + "_"))
 print(f"SeqNum {len(filtered)}")
